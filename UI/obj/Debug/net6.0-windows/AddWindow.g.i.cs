b91c2d8e4780a64e705b0a1116f7524d7be283fb
c1ose-d <michael.glodin2004@gmail.com>
2023-03-24 23:57:57 +0300

    Fix 0.3.4

--- UI/obj/Debug/net6.0-windows/AddWindow.g.i.cs
+++ UI/obj/Debug/net6.0-windows/AddWindow.g.i.cs
@@ -92,7 +92,7 @@ namespace UI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UI;component/addwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UI;V1.0.0.0;component/addwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AddWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- UI/obj/Debug/net6.0-windows/App.g.i.cs
+++ UI/obj/Debug/net6.0-windows/App.g.i.cs
@@ -58,7 +58,7 @@ namespace ParsethingTasks {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/UI;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
